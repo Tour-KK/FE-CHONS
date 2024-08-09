2a66fca140fccd6c932732ee715cbc2cbab948ef
cho-byeong-gyu <chobyoenggyu03@gmail.com>
2024-08-09 22:50:38 +0900

    구글 로그인 api 코드 다듬음 + 구글 지도 API KEY값 변경

--- GoogleAPI.js
+++ GoogleAPI.js
@@ -3,25 +3,32 @@ import { View, StyleSheet, Button } from 'react-native';
 import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-GoogleSignin.configure({
-  webClientId: '412626397279-eg75216s42no729d1cmftmkns983ouhq.apps.googleusercontent.com',
+GoogleSignin.configure({										// 구글 로그인 서비스 초기화
+  webClientId: '412626397279-lhjdpoasbnh9qejao9v9fkl6hsempkkl.apps.googleusercontent.com',
+  offlineAccess: true
 });
 
 const GoogleAPIScreen = () => {
-  const googleSignin = async () => {
+  const googleSignin = async () => {							// 구글 계정 로그인 함수
     try {
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      console.log(userInfo);
+      await GoogleSignin.hasPlayServices(); 					// 안드로이드 기기에서 Google Play 서비스를 사용할 수 있는지 확인
+      const userInfo = await GoogleSignin.signIn();				// Google 계정선택 로그인창 표시
+      console.log(userInfo);									// 로그인 한 사용자 데이터 확인용 테스트 코드
+      alert("이름: " + userInfo.user.name +"\n" 
+        +"이메일: "+ userInfo.user.email +"\n" 
+        +"id: "+ userInfo.user.id +"\n" 
+        +"socialType: GOOGLE \n" 
+        +"프로필 사진: "+ userInfo.user.photo +"\n"
+      );
     } catch (error) {
       console.error(error);
     }
   };
 
-  const googleSignout = async () => {
+  const googleSignout = async () => {							// 구글 계정 로그아웃 함수
     try {
-      await GoogleSignin.signOut();
-      console.log('User signed out');
+      await GoogleSignin.signOut();								// 구글 계정 로그아웃처리후, 로컬 세션 제거
+      alert('로그아웃되었습니다.');								// 로그아웃처리 제대로 완료되었는지 테스트 코드
     } catch (error) {
       console.error(error);
     }
